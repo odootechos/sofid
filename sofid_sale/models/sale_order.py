369489cbe1862132f2c5f0a156bb0fa8ad3dee16
tsiry-odoo-etech <t.barinesa@etechconsulting-mg.com>
2025-02-20 11:38:03 +0300

    [IMP] ticket_6 create wedget for float field to allow only integer

--- sofid_sale/models/sale_order.py
+++ sofid_sale/models/sale_order.py
@@ -8,11 +8,12 @@ class SaleOrder(models.Model):
     
     purchase_order_ids = fields.Many2many('purchase.order')
    
-class SaleOrderLine(models.Model):
-    _inherit = 'sale.order.line'
+# class SaleOrderLine(models.Model):
+#     _inherit = 'sale.order.line'
 
-    @api.constrains('product_uom_qty')
-    def _check_product_uom_qty(self):
-        for record in self:
-            if not isinstance(record.product_uom_qty, int) or record.product_uom_qty != int(record.product_uom_qty):
-                raise ValidationError(_("Please re-enter the quantity: this field must be an integer."))
+#     @api.constrains('product_uom_qty')
+#     def _check_product_uom_qty(self):
+#         for record in self:
+#             import pudb;pudb.set_trace()
+#             if not isinstance(record.product_uom_qty, int) or record.product_uom_qty != int(record.product_uom_qty):
+#                 raise ValidationError(_("Please re-enter the quantity: this field must be an integer."))
