4c2c85b10a00eb5ec6ac79f8618f0b441fcefdb2
tsiry <t.barinesa@etechconsulting-mg.com>
2025-12-12 10:38:28 +0300

    [IMP] HT09468 : Update sofid_dgi

--- sofid_dgi/__manifest__.py
+++ sofid_dgi/__manifest__.py
@@ -6,8 +6,11 @@
     "summary": "Intégration FNE pour les factures client",
     "depends": ["account"],
     "data": [
+        "security/ir.model.access.csv",
         "data/ir_config_parameter.xml",
         "views/account_move_view.xml",
+        "views/dgi_send_confirm_wizard_views.xml",
+        "views/dgi_send_refund_confirm_wizard_views.xml",
         "views/report_invoice_inherit.xml",
     ],
     "assets": {
@@ -17,5 +20,4 @@
     },
     "installable": True,
     "application": False,
-    "license": "LGPL-3",
 }
