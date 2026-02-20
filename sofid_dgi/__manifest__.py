23f168456a48ed710cf5c6dcbca1f67d1abb5b49
h.andrisoa <h.andrisoa@etechconsulting-mg.com>
2026-02-20 13:49:53 +0300

    [FIX] path for line.discount element

--- sofid_dgi/__manifest__.py
+++ sofid_dgi/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "SOFID FNE",
-    "version": "16.0.1.0.0",
+    "version": "1.0",
     "category": "Accounting",
     "author": "SOFID",
     "summary": "Intégration FNE pour les factures client",
