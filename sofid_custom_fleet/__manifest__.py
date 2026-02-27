20559399d242b72398926785b49544c01d2e4dfd
h.andrisoa <h.andrisoa@etechconsulting-mg.com>
2026-02-27 11:24:26 +0300

    [IMPL] Update sofid_custom_fleet module

--- sofid_custom_fleet/__manifest__.py
+++ sofid_custom_fleet/__manifest__.py
@@ -11,6 +11,7 @@
     'data': [
         'security/ir.model.access.csv',
         'views/fleet_vehicle_views.xml',
+        'views/fleet_employee_inherit.xml',
         'views/fleet_vehicle_model_views.xml',
         'views/fleet_fuel_log_views.xml'
     ],
