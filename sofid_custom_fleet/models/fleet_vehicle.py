22770c80b7ec85404afa3ca58fba33d6877a5b33
h.andrisoa <h.andrisoa@etechconsulting-mg.com>
2026-03-04 15:36:31 +0300

    [IMPL] sofid_custom_fleet update

--- sofid_custom_fleet/models/fleet_vehicle.py
+++ sofid_custom_fleet/models/fleet_vehicle.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields,api
 
 class FleetVehicle(models.Model):
     _inherit = 'fleet.vehicle'
@@ -25,11 +25,55 @@ class FleetVehicle(models.Model):
         readonly=True
     )
 
+    @api.onchange('employee_driver_id')
+    def _onchange_employee_driver_id(self):
+        for rec in self:
+            if rec.employee_driver_id and rec.employee_driver_id.address_home_id:
+                rec.driver_id = rec.employee_driver_id.address_home_id
+
+    @api.model
+    def create(self, vals):
+        if vals.get('employee_driver_id'):
+            employee = self.env['hr.employee'].browse(vals['employee_driver_id'])
+            if employee.address_home_id:
+                vals['driver_id'] = employee.address_home_id.id
+        vehicle = super(FleetVehicle, self).create(vals)
+        # Création automatique du log si conducteur défini
+        if vehicle.employee_driver_id:
+            self.env['fleet.vehicle.assignation.log'].create({
+                'vehicle_id': vehicle.id,
+                'employee_driver_id': vehicle.employee_driver_id.id,
+                'driver_id': vehicle.driver_id.id,
+                'date_start': fields.Date.today(),  # date de début de l'assignation
+            })
+        return vehicle
+
+    def write(self, vals):
+        # remplir driver_id avant écriture
+        if 'employee_driver_id' in vals and vals['employee_driver_id']:
+            employee = self.env['hr.employee'].browse(vals['employee_driver_id'])
+            if employee.address_home_id:
+                vals['driver_id'] = employee.address_home_id.id
+
+        res = super(FleetVehicle, self).write(vals)
+
+        # créer un log si employee_driver_id changé
+        if 'employee_driver_id' in vals:
+            for vehicle in self:
+                self.env['fleet.vehicle.assignation.log'].create({
+                    'vehicle_id': vehicle.id,
+                    'employee_driver_id': vehicle.employee_driver_id.id,
+                    'driver_id': vehicle.driver_id.id,
+                    'date_start': fields.Date.today(),
+                })
+        return res
+
+
     def _compute_fuel_log_count(self):
-        for record in self:
-            record.fuel_log_count = self.env['sofid.fleet.fuel.log'].search_count([
-                ('vehicle_id', '=', record.id)
-            ])
+            for record in self:
+                record.fuel_log_count = self.env['sofid.fleet.fuel.log'].search_count([
+                    ('vehicle_id', '=', record.id)
+                ])
 
     def open_fuel_logs(self):
         self.ensure_one()
@@ -80,3 +124,27 @@ class FleetVehicleAssignationLog(models.Model):
         string='Conducteur',
         tracking=True
     )
+    driver_id = fields.Many2one(
+        'res.partner',
+        string='Conducteur',
+        tracking=True,
+        required=False,
+    )
+
+    @api.model
+    def create(self, vals):
+        # remplir driver_id à partir de employee_driver_id
+        if 'employee_driver_id' in vals and vals['employee_driver_id']:
+            employee = self.env['hr.employee'].browse(vals['employee_driver_id'])
+            if employee.address_home_id:
+                vals['driver_id'] = employee.address_home_id.id
+        return super(FleetVehicleAssignationLog, self).create(vals)
+
+    def write(self, vals):
+        # idem pour write
+        if 'employee_driver_id' in vals and vals['employee_driver_id']:
+            employee = self.env['hr.employee'].browse(vals['employee_driver_id'])
+            if employee.address_home_id:
+                vals['driver_id'] = employee.address_home_id.id
+        return super(FleetVehicleAssignationLog, self).write(vals)
+
